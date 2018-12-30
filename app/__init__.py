22219b36031abeda3e0beb2be2ce3ce724043a0b
cmiu007 <cmiu007@gmail.com>
2018-12-30 19:09:23 +0200

    update

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,9 @@ bus = SMBus(1)
 bufallo = 0x48
 
 def read_reg(reg_no):
-    bus.write_byte(bufallo, reg_no)
-    val = "{0:08b}".format(bus.read_byte(bufallo))
+    # bus.write_byte(bufallo, reg_no)
+    # val = "{0:08b}".format(bus.read_byte(bufallo))
+    val = "{0:08b}".format(bus.read_byte_data(bufallo, reg_no))
     return val
 
 print(read_reg(10))
