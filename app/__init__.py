a57c3eb3385d9741c33bbaa924922654e1c09b59
cmiu007 <cmiu007@gmail.com>
2018-12-30 19:06:54 +0200

    update

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ bufallo = 0x48
 
 def read_reg(reg_no):
     bus.write_byte(bufallo, reg_no)
-    val = "{0:08}".format(bus.read_byte(bufallo))
+    val = "{0:08b}".format(bus.read_byte(bufallo))
     return val
 
 print(read_reg(10))
