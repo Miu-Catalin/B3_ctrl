42d46a78e8626998a591cca6f252366a6bbe143a
cmiu007 <cmiu007@gmail.com>
2018-12-30 19:25:11 +0200

    update

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from smbus2 import SMBus, SMBusWrapper, i2c_msg
+from bitstring import BitArray
 
 bus = SMBus(1)
 bufallo = 0x48
@@ -6,7 +7,7 @@ bufallo = 0x48
 def read_reg(reg_no):
     # bus.write_byte(bufallo, reg_no)
     # val = "{0:08b}".format(bus.read_byte(bufallo))
-    val = bus.read_byte_data(bufallo, reg_no)
+    val = BitArray(bus.read_byte_data(bufallo, reg_no))
     return val
 
 print(read_reg(10))
