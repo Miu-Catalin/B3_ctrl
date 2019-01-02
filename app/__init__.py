f8409d957c73ae89f91dbb779befd4df5f456bb3
cmiu007 <cmiu007@gmail.com>
2019-01-02 11:49:52 +0200

    update

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 from app.helpers import read_reg
 
-a = read_reg(10)
-print(a)
-print(a.bin)
-print(a[0:2])
-print(a[2:4])
-print(a[4:6])
-print(a[6:8])
+ret_code, a = read_reg(10)
+if ret_code == 0:
+    print(a)
+    print(a.bin)
+    print(a[0:2])
+    print(a[2:4])
+    print(a[4:6])
+    print(a[6:8])
+else:
+    print('error')
